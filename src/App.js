fa2de221e78269eb86d2d68792efea952657efd6
ludovic Vachon <vachon.ludovic@gmail.com>
2021-04-27 22:28:34 +0200

    MAJ

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import './tsFlow';
+import {ML} from './tsFlow';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
+      <h1 style={{color:"white"}}>q32sd1q3d</h1>
+      <h1 style={{color:"green"}}>q32sd1q3d</h1>
+      <ML/>
     </div>
   );
 }
